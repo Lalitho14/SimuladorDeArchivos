1b470e41786313e6fefd50feae409dc78ef5b46f
Lalitho <lalithohb@gmail.com>
2023-10-26 23:11:01 -0600

    ImplementarGUIpart1.1

---
+++ src/MainGui.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class MainGui extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Escenas/menu.fxml")));
+    stage.setTitle("Simulador de Archivos");
+    stage.setScene(new Scene(root, 800, 600));
+    stage.setWidth(800);
+    stage.setHeight(600);
+    stage.setMinWidth(800);
+    stage.setMinHeight(600);
+    stage.show();
+  }
+}
